062311882468ce4ea3df6327dd396934ddf84abe
Sean Kelly <shonkelli@gmail.com>
2019-02-03 16:17:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(input)
   if input == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif input = input.upcase
-    "HUH?! SPEAK UP, SONNY!"
+    "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
   end
